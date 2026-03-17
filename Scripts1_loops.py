41c4e694090c1af93823f0f9574f431bcabe3adb
Krrish Malhan <ks.industries2024@gmail.com>
2026-03-17 23:23:06 +0530

    Refactor transaction processing and add continue logic
    Updated balance calculation logic and added continue statement to skip transactions below 500.

--- Scripts1_loops.py
+++ Scripts1_loops.py
@@ -15,20 +15,23 @@ transaction=[250,1500,89,4200,73000]
 i=0
 balance=100000
 while balance >=50000:
+    balance=balance-transaction[i]
     if balance < 50000:
         break
-    balance=balance-transaction[i]
+    print(balance)
     i=i+1
-
 
 #task3:add continue:loop over the same transaction list.skip any amount below 500
 #using continue.print the rest.
 #program-
 transaction=[250,1500,89,4200,73000]
+balance=10000
 for trans in transaction:
-    if trans<=500:
+    if trans <= 500:      # 1. decide: skip or not
         continue
-    balance=balance-trans
-    print(balance)
+    balance = balance - trans  # 2. only operate on what passed the filter
+    print(balance)             # 3. show result
+    
+#donot put condition before first possible operation so the variable should get new data after operation then check it sometimes allow first operation costly apply condition first
     
         
